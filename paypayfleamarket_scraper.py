7f3a566a979fca6768c1bd3091738d7dcadea015
shete0804 <shete0804@gmail.com>
2026-09-16 14:27:26 +0900

    Update paypayfleamarket_scraper.py

--- paypayfleamarket_scraper.py
+++ paypayfleamarket_scraper.py
@@ -122,6 +122,7 @@ def search_card(keyword: str) -> list[Listing]:
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
 
+                chrome_options.binary_location = "/snap/bin/chromium"
         driver = webdriver.Chrome(options=chrome_options)
 
         try:
